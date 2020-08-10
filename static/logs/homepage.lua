09dd9f27b33cbf391283d500c198fc700b1cf39c
Pancakeddd <chaneofcake@gmail.com>
2020-08-10 17:51:49 -0400

    Made homepage more interesting

--- static/logs/homepage.lua
+++ static/logs/homepage.lua
@@ -1,1 +1,15 @@
-do local _={};return _;end
+do local _={{color="#000000",message=[[Pestergen is a Homestuck page generator and host that supports uploading images, GIFs, and color logs. It uses the Homestuck style sheets so that it looks as close as possible to the original.]]}, 
+{color="#000000",message=[[    
+
+
+  ]]},
+{color="#000000",message=[[
+Made by kankri and zuup.
+https://github.com/daelvn/pestergen]]},
+{color="#000000",message=[[    
+
+
+]]},
+{color="#000000",message=[[    
+Click "Create New" to create a new panel.
+Click "View All" to get a list of all uploaded panels.]]}};return _;end
